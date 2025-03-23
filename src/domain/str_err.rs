a4241c2623093ad30c2d8ae6c9dee6e9374c9c81
Anton Lobanov <a.givertzman@icloud.com>
2025-03-23 16:57:33 +0300

    StrErr | initial

--- src/domain/str_err.rs
+++ src/domain/str_err.rs
@@ -1,1 +1,46 @@
-pub struct StrErr {}
+///
+/// 
+pub struct StrErr {
+    name: String,
+    msg: String,
+    child: Option<Box<StrErr>>,
+}
+//
+//
+impl StrErr {
+    ///
+    /// Returns [StrErr] new root instance, without child
+    /// 
+    /// use pass(child) to wrap incoming error
+    pub fn new(msg: impl Into<String>) -> Self {
+        Self {
+            msg: msg.into(),
+            child: None,
+        }
+    }
+    ///
+    /// 
+    pub fn pass(child: Self) -> Self {
+        Self {
+            msg: String::new(),
+            child: Some(Box::new(child)),
+        }
+    }
+}
+// Error doesn't require you to implement any methods, but
+// your type must also implement Debug and Display.
+impl std::error::Error for StrErr {}
+//
+//
+impl std::fmt::Display for StrErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Delegate to the Display impl for `&str`:
+        self.0.fmt(f)
+    }
+}
+impl std::fmt::Debug for StrErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Delegate to the Display impl for `&str`:
+        std::fmt::Display::fmt(&self, f)
+    }
+}
